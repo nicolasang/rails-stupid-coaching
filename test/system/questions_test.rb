36d75f7eea89c7b233bdb3f77ad76ae748e3722c
Nicolas Ang <nicolas-ang@outlook.com>
2018-11-05 13:50:22 +1100

    completed with tests

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -1,23 +1,23 @@
 require "application_system_test_case"
-# require 'capybara'
+require 'capybara'
 
 class QuestionsTest < ApplicationSystemTestCase
 
   test "visiting /ask renders the form" do
     visit ask_url
     assert_selector "p", text: "Ask your coach anything!"
+    save_and_open_screenshot # the equivalent of binding.pry
   end
 
-  # take_screenshot # the equivalent of binding.pry
 
   test "saying Hello yields a grumpy response from the coach" do
     visit ask_url
     fill_in "answer_input", with: "Hello"
     click_on "Ask"
 
+    save_and_open_screenshot
     assert_text "I don't care, get dressed and go to work!"
   end
 
-  # take_screenshot
 
 end
